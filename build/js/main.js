ca28ad9e9e8b9201aaa3bf0dd116c1e7d92f957e
ApuriniAz <az@apurini.io>
2022-01-16 22:11:25 +0200

    main.js commented

--- build/js/main.js
+++ build/js/main.js
@@ -1,19 +1,19 @@
 function main() {
 
-  (function () {
-    'use strict';
+  // (function () {
+  //   'use strict';
 
-    // Hide nav on click 
-    $(document).ready(function (e) { // TODO: To be Removed
-      $(".navbar-nav li a").click(function (event) {
-        // check if window is small enough so dropdown is created
-        var toggle = $(".navbar-toggle").is(":visible");
-        if (toggle) {
-          $(".navbar-collapse").collapse('hide');
-        }
-      });
-    });
-  }());
+  //   // Hide nav on click 
+  //   $(document).ready(function (e) { // TODO: To be Removed
+  //     $(".navbar-nav li a").click(function (event) {
+  //       // check if window is small enough so dropdown is created
+  //       var toggle = $(".navbar-toggle").is(":visible");
+  //       if (toggle) {
+  //         $(".navbar-collapse").collapse('hide');
+  //       }
+  //     });
+  //   });
+  // }());
 
 }
 main();
